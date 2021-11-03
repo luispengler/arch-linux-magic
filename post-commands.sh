bd1128877b43afff3e01783684ced4b2d723ac5f
Luis Spengler <luispengler@protonmail.com>
2021-11-02 21:53:23 -0400

    Update post-commands.sh

--- post-commands.sh
+++ post-commands.sh
@@ -4,4 +4,7 @@ setxkbmap -model thinkpad -layout br
 pacman -S --noconfirm discord ripgrep fd
 
 ## Add entry for doom emacs.
+## git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+## ~/.emacs.d/bin/doom install
+
 ## Create a way to download app images. e.g some wget or curl.
