f3a711bc9cf333aad17b39458bfa1253b1b12220
Luis Spengler <luispengler@protonmail.com>
2021-11-08 07:54:43 -0400

    adding video-intel

--- post-commands.sh
+++ post-commands.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 setxkbmap -model thinkpad -layout br
-pacman -S --noconfirm discord ripgrep fd
+pacman -S --noconfirm discord ripgrep fd xf86-video-intel
 
 ## Add entry for doom emacs.
 ## git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
