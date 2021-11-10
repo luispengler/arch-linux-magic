88f6f47b3967450bd5ce701457c65f61d092b127
Luis Spengler <luispengler@protonmail.com>
2021-11-10 19:39:40 -0400

    Adding better discord

--- post-commands.sh
+++ post-commands.sh
@@ -9,8 +9,13 @@ baph -ianN yay brave-bin #Installs brave and yay
 
 ## Downloading AppImages
 # You should make multiple connections to get faster download
+cd ~/lg/appimages
+mkdir kdenlive
 cd ~/lg/appimages/kdenlive
 wget https://download.kde.org/stable/kdenlive/21.08/linux/kdenlive-21.08.3-x86_64.appimage
+mkdir betterdiscord
+cd ~/lg/appimages/betterdiscord
+wget https://github.com/BetterDiscord/Installer/releases/latest/download/BetterDiscord-Linux.AppImage
 
 ## Git Cloning
 cd ~/lg/gitclones/
