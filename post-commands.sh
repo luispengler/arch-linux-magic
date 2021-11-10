1c9246880fac909e2dc2a119e439d288c5f3b734
Luis Spengler <luispengler@protonmail.com>
2021-11-10 15:02:06 -0400

    adding brave and yay

--- post-commands.sh
+++ post-commands.sh
@@ -2,7 +2,7 @@
 
 setxkbmap -model thinkpad -layout br
 pacman -S --noconfirm discord ripgrep fd 
-
+baph -ianN yay brave-bin #Installs brave and yay
 ## Add entry for doom emacs.
 ## git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ## ~/.emacs.d/bin/doom install
