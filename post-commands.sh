c29ca3c6284af9f55c3a00fe96fcca05d010bef8
Luis Spengler <luispengler@protonmail.com>
2021-11-11 18:35:01 -0400

    Adding doom emacs

--- post-commands.sh
+++ post-commands.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 setxkbmap -model thinkpad -layout br
-pacman -S --noconfirm emacs discord thunar gvfs transmission-gtk kdeconnect wget python2 python-pip xdg-user-dirs ripgrep fd
+pacman -S --noconfirm emacs ripgrep fd discord thunar gvfs transmission-gtk kdeconnect wget python2 python-pip xdg-user-dirs
 baph -ianN yay brave-bin #Installs brave and yay
+
 ## Add entry for doom emacs.
-## git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-## ~/.emacs.d/bin/doom install
+cd ~/lg/gitclones
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
 
 ## Installing LaTeX stuff
 pacman -S textlive-most texlive-lang
@@ -26,9 +28,6 @@ cd ~/lg/appimages
 mkdir kdenlive
 cd ~/lg/appimages/kdenlive
 wget https://download.kde.org/stable/kdenlive/21.08/linux/kdenlive-21.08.3-x86_64.appimage
-mkdir betterdiscord
-cd ~/lg/appimages/betterdiscord
-wget https://github.com/BetterDiscord/Installer/releases/latest/download/BetterDiscord-Linux.AppImage
 
 ## Git Cloning
 cd ~/lg/gitclones/ ## Firefox secure profile
